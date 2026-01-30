cdd2677355c8fc623581c59f4a6af44302afdee8
LucyEmma22 <lucy.binsted@ed.ac.uk>
2026-01-30 18:08:08 +0000

    Add comments to day 6

--- day6.py
+++ day6.py
@@ -14,22 +14,22 @@ with open("Documents/AdventOfCode25/input_day6.txt") as f:
     data = f.read().splitlines()
 
 # Part 1
-data1 = [re.sub(r'\s+', ' ', s).strip() for s in data]
-df = pd.DataFrame(data1)[0].str.split(" ", expand=True).T
-numeric_cols = df.iloc[:, :-1].astype(int)
-df['result'] = np.where(df[4] == '+', numeric_cols.sum(axis=1), numeric_cols.prod(axis=1))
+data1 = [re.sub(r'\s+', ' ', s).strip() for s in data] # Remove extra spaces
+df = pd.DataFrame(data1)[0].str.split(" ", expand=True).T # Convert list of strings to dataframe. Split strings into different columns by spaces. Transpose dataframe
+numeric_cols = df.iloc[:, :-1].astype(int) # Define numeric columns (not column containing * and +)
+df['result'] = np.where(df[4] == '+', numeric_cols.sum(axis=1), numeric_cols.prod(axis=1)) # Create new column with the sum or product of numeric columns (depending on + or *)
 print(sum(df['result']))
 
 # Part 2
-separator_positions = [i-1 for i, x in enumerate(data[-1]) if x in ('+', '*')][1:]
-data2 = [''.join('-' if i in separator_positions else c for i, c in enumerate(x)) for x in data]
-data2 = [x.split("-") for x in data2]
+separator_positions = [i-1 for i, x in enumerate(data[-1]) if x in ('+', '*')][1:] # Determine positions of + or * (this is where the new problem starts)
+data2 = [''.join('-' if i in separator_positions else c for i, c in enumerate(x)).split("-") for x in data] # Split strings at the position before + or *
 count = 0
 for i in range(len(data2[0])):
-    df = pd.DataFrame(pd.DataFrame([lst[i] for lst in data2[:-1]])[0].astype(str).apply(list).tolist()).T
-    df['number'] = df.astype(str).agg(''.join, axis=1).astype(int)
+    col_values = [lst[i] for lst in data2[:-1]] # Get numbers from a single problem
+    df = pd.DataFrame([list(str(x)) for x in col_values]).T # Create a dataframe with each number split into seperate columns. Transpose dataframe
+    df['number'] = df.astype(str).agg(''.join, axis=1).astype(int) # Add a column that joins the transposed numbers into a single integer
     if "+" in data2[-1][i]:
-        count = count + df['number'].sum()
+        count = count + df['number'].sum() # Add sum of numbers if +
     else:
-        count = count + df['number'].prod()
+        count = count + df['number'].prod() # Add product of numbers if *
 print(count)
