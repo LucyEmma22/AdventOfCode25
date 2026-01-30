4ecd8c4e429e56d4967da439dfd8071678cfc6bf
LucyEmma22 <lucy.binsted@ed.ac.uk>
2026-01-30 19:19:26 +0000

    Add comments to day 1

--- day1.py
+++ day1.py
@@ -17,49 +17,43 @@ test_data = pd.DataFrame({0:["L68",
 data = pd.read_csv("Documents/AdventOfCode25/input_day1.txt", header=None)
 
 # Part 1
-position = 50
+position = 50                                                                  # Start at position 50
 count = 0
-positions = []
 for i in range(len(data)):
-    direction = data[0].str[0][i]
-    distance = data[0].str[1:].astype(int)[i] % 100
+    direction = data[0].str[0][i]                                              # Get the direction
+    distance = data[0].str[1:].astype(int)[i] % 100                            # Get the distance (keep only the last 2 digits as a distance of 100 ends in the same position)
     if direction == "R":
-        position = position + distance
-        if position > 99:
-            position = position - 100
+        position = position + distance                                         # If the direction is right, add the distance to the current position
+        if position > 99:                                                      # If thre result is greater than 99...
+            position = position - 100                                          # Take 100 from the result to get the actual position (since this means it has done a full rotation and has crossed 0 forwards)
     else:
-        position = position - distance
-        if position < 0:
-            position = 100 + position
-    if position == 0:
+        position = position - distance                                         # If the direction is right, take the distance away from the current position
+        if position < 0:                                                       # If thre result is less than 0...
+            position = 100 + position                                          # Add 100 to the result to get the actual position (since this means it has done a full rotation and has crossed 0 backwards)
+    if position == 0:                                                          # If it lands on 0, add 1 to the count
         count = count + 1
-    positions.append(position)
 print(count)
 
 # Part 2
-position = 50
+position = 50                                                                  # Start at position 50
 count = 0
-counts = []
-positions = []
 for i in range(len(data)):
-    direction = data[0].str[0][i]
-    distance = data[0].str[1:].astype(int)[i] % 100
-    count = count + data[0].str[1:].astype(int)[i] // 100
+    direction = data[0].str[0][i]                                              # Get the direction
+    distance = data[0].str[1:].astype(int)[i] % 100                            # Get the distance (keep only the last 2 digits as a distance of 100 ends in the same position)
+    count = count + data[0].str[1:].astype(int)[i] // 100                      # If the distance is more than 100, add the number of times is passes 0 to the count
     prev_position = position
     if direction == "R":
-        position = position + distance
-        if position > 99:
-            position = position - 100
-            if prev_position != 0 and position !=0:
+        position = position + distance                                         # If the direction is right, add the distance to the current position
+        if position > 99:                                                      # If thre result is greater than 99...
+            position = position - 100                                          # Take 100 from the result to get the actual position (since this means it has done a full rotation and has crossed 0 forwards)
+            if prev_position != 0 and position !=0:                            # If the dial did not start or end exactly on 0, add 1 to the count
                 count = count + 1
     else:
-        position = position - distance
-        if position < 0:
-            position = 100 + position
-            if prev_position != 0 and position != 0:
+        position = position - distance                                         # If the direction is right, take the distance away from the current position
+        if position < 0:                                                       # If thre result is less than 0...
+            position = 100 + position                                          # Add 100 to the result to get the actual position (since this means it has done a full rotation and has crossed 0 backwards)
+            if prev_position != 0 and position != 0:                           # If the dial did not start or end exactly on 0, add 1 to the count
                 count = count + 1
-    if position == 0:
-        count = count + 1
-    positions.append(position)
-    counts.append(count)
+    if position == 0:                                                          # If it lands exactly on 0, add 1 to the count
+        count = count + 1 
 print(count)
